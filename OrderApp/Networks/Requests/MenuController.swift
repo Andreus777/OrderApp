506a000925968c4d368616f20a03d2a7e273f358
Andrew <Andreu_s@tut.by>
2021-11-04 17:34:02 +0300

    add new class to request data

---
+++ OrderApp/Networks/Requests/MenuController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuController.swift
+//  OrderApp
+//
+//  Created by Андрей Фокин on 4.11.21.
+//
+
+import Foundation
+
+class MenuController {
+    
+    let baseUrl = URL(string: "http://localhost:8080/")!
+    
+    
+    
+    
+    
+    
+}
