00da43618e900e3f0861fffc3a6b4b09ea533890
Andrew <Andreu_s@tut.by>
2021-11-09 22:25:31 +0300

    Configure cell for categories

--- OrderApp/Controllers/CategoryTableViewController.swift
+++ OrderApp/Controllers/CategoryTableViewController.swift
@@ -9,38 +9,66 @@ import UIKit
 
 class CategoryTableViewController: UITableViewController {
 
+    let menuController = MenuController()
+    var categories: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        menuController.fetchingCategories { result in
+            switch result {
+            case .success(let categories):
+                self.updateUI(with: categories)
+            case .failure(let error):
+                self.displayError(error, title: "Can't fethcing categories")
+            }
+        }
     }
 
+    func updateUI(with categories: [String]){
+        DispatchQueue.main.async {
+            self.categories = categories
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    func displayError(_ error: Error, title: String){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            self.present(alert, animated: true, completion: nil)
+        
+        }
+    }
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Category", for: indexPath)
+        updateCell(with: cell, categories: indexPath)
         return cell
     }
-    */
 
+    
+    func updateCell(with cell: UITableViewCell, categories indexPath: IndexPath){
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.capitalized
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
